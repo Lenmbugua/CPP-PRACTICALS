0db8d17fb35f8dd40b3dbc04e9f574ad7023cd29
Lenmbugua <92791059+Lenmbugua@users.noreply.github.com>
2025-02-19 09:50:43 +0300

    Variables in C++

--- variables.cpp
+++ variables.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-    int num1 ;
-    float num2,num3 ;
-    int num4 = 1 ;
-    int num5,num6 = 2 ;
+    int num1;
+    float num2,num3;
+    int num4 = 1;
+    int num5,num6 = 2;
     cout << "num1 = "<<num1<< endl << "num2 = " <<num2<< endl << "num3" <<num3 <<endl << "num4= " <<num4 << endl << "num5=" <<num5  << endl << "num6 =" <<num6 << endl;
     return 0;   
 }
