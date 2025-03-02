4d3b82f04deae1c3078e61c3edf6731eedbdaf9f
Lenmbugua <92791059+Lenmbugua@users.noreply.github.com>
2025-03-02 15:57:35 +0300

    implementation of member values

--- class2.cpp
+++ class2.cpp
@@ -1,19 +1,59 @@
 #include <iostream>
 using namespace std;
-class cat 
-{
-    public:  void setAge (int age);
-             int GetAge();
-             void setWeight (int weight);
-             int Getweight();
-    private: int itsAge();
-             int itsWeight();
+
+class Cat {
+public:
+    void setAge(int age);
+    int getAge();
+    
+    void setWeight(int weight);
+    int getWeight();
+
+private:
+    int itsAge = 0;     // Initial values
+    int itsWeight = 0;
 };
-int cat::GetAge()
-{
+
+// Implementation of member functions
+void Cat::setAge(int age) {
+    itsAge = age;
+}
+
+int Cat::getAge() {
     return itsAge;
 }
-void cat::setAge(int age)
-{
-    itsAge = age;
+
+void Cat::setWeight(int weight) {
+    itsWeight = weight;
+}
+
+int Cat::getWeight() {
+    return itsWeight;
+}
+
+int main() {
+    Cat Felix;
+
+    // Print initial values
+    cout << "Initial status of Felix:" << endl;
+    cout << "Age: " << Felix.getAge() << " years" << endl;
+    cout << "Weight: " << Felix.getWeight() << " kg" << endl;
+
+    // Get new values from user
+    int newAge, newWeight;
+    cout << "\nEnter new age for Felix: ";
+    cin >> newAge;
+    cout << "Enter new weight for Felix: ";
+    cin >> newWeight;
+
+    // Update values
+    Felix.setAge(newAge);
+    Felix.setWeight(newWeight);
+
+    // Print updated values
+    cout << "\nUpdated status of Felix:" << endl;
+    cout << "Age: " << Felix.getAge() << " years" << endl;
+    cout << "Weight: " << Felix.getWeight() << " kg" << endl;
+
+    return 0;
 }
