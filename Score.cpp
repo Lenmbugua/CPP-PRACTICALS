2887ab1d6d20853bf8dba4db5861c3a21d28c495
Lenmbugua <92791059+Lenmbugua@users.noreply.github.com>
2025-03-11 09:42:22 +0300

    Past papers revision

---
+++ Score.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int creditscore;
+    cout << "Enter the credit score of the applicant: " << endl;
+    cin >> creditscore ;
+    switch (creditscore) {
+        case 3:
+        cout << "Amount: 15,000" << endl;
+        case 2:
+        cout << "Amount: 10,000" << endl;
+        case 1:
+        cout << "Amount: 0" <<endl;
+        break;
+        default:
+        cout << "invalid credit score entered. " << endl;
+    }
+    return 0;
+}
